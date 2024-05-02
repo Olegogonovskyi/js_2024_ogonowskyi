47721473878507c795414726f98cc34a4e02360d
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-05-02 16:32:05 +0300

    train_3-routes: watch second & third part of route

--- src/Pages/User/User.tsx
+++ src/Pages/User/User.tsx
@@ -5,6 +5,7 @@ const User: FC<IUsersmodel> = ({id, username, name}) => {
     return (
         <div>
             <h3>{id}: {name} - {username}</h3>
+            <button onClick={} >what he said</button>
         </div>
     );
 };
