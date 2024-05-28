b956dfab54f051e37cd62d8998aeda124cd01eda
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-05-28 19:49:54 +0300

    train-auth-first: get cars, todo refresh

--- src/Components/CarsComponent/CarsComponent.tsx
+++ src/Components/CarsComponent/CarsComponent.tsx
@@ -1,14 +1,19 @@
-import React, {FC, useEffect} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {carsService} from "../../services/carsService";
+import {ICarWithAuth} from "../../models/ICarWithAuth";
+import CarComponent from "../CarComponent/CarComponent";
 
 const CarsComponent:FC = () => {
+    const [cars, setCars] = useState<ICarWithAuth[]>()
 
     useEffect(() => {
-        carsService.getCars().then(value => console.log(value))
+        carsService.getCars().then(value => setCars(value.items))
     }, []);
     return (
         <div>
-fff
+            {
+                cars && cars.map((car, index) => <CarComponent car={car} key={index}/> )
+            }
         </div>
     );
 };
