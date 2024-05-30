7ee24c1e7dd90f1ec9cafebd2fb05022d594f7ca
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-05-30 10:36:25 +0300

    hw-9-Auth: make LoginFormComponent.tsx

--- src/Componnts/LoginFormComponent/LoginFormComponent.tsx
+++ src/Componnts/LoginFormComponent/LoginFormComponent.tsx
@@ -1,12 +1,18 @@
 import React, {FC} from 'react';
 import {useForm} from "react-hook-form";
+import {ITokenObtainPairModel} from "../../Models/ITokenObtainPairModel/ITokenObtainPairModel";
 
 const LoginFormComponent: FC = () => {
-    const {} = useForm()
+    const {register, handleSubmit} = useForm<ITokenObtainPairModel>()
+    const sendUserData = async (userFormData: ITokenObtainPairModel) => {
+        await console.log(userFormData)
+    }
     return (
         <div>
-            <form>
-                <input type="text"/>
+            <form onSubmit={handleSubmit(sendUserData)}>
+                <input type="text" {...register('username')}/>
+                <input type="text" {...register('password')}/>
+                <button>login</button>
             </form>
         </div>
     );
